7fd0d5a32dab75cf5b759dfad41ed5a5f878b0e8
DenisYagov <denis.yagov@yandex.ru>
2022-06-21 10:57:37 +0500

    tests modified

--- __tests__/coreComporator.test.js
+++ __tests__/coreComporator.test.js
@@ -7,27 +7,19 @@ const f1 = '__fixtures__/file1.json';
 const f2 = '__fixtures__/file2.json';
 const f3 = '__fixtures__/file3.json';
 
-test('simple compare same files', () => {
-    const rawData = {
-        args : [f1 , f1]};
-    const rez1 = readFileSync('__fixtures__/rez1').toString();
-    expect(flatCompare(rawData)).toEqual(rez1);
-  });
+const comparation = (file1, file2, eqFile) => {
+  const rawData = {
+    args : [file1 , file2]};
+  const rez1 = readFileSync('__fixtures__/' + eqFile).toString();
+  expect(flatCompare(rawData)).toEqual(rez1);
+}
 
-test('compare with empty files', () => {
-    const rawData = {
-        args : [f1 , f2]};
-    const rez21 = readFileSync('__fixtures__/rez21').toString();
-    expect(flatCompare(rawData)).toEqual(rez21);
-    rawData.args[0] = f2;
-    rawData.args[1] = f1;
-    const rez22 = readFileSync('__fixtures__/rez22').toString();
-    expect(flatCompare(rawData)).toEqual(rez22);
-  });
-
-  test('simple compare different files', () => {
-    const rawData = {
-        args : [f1 , f3]};
-    const rez3 = readFileSync('__fixtures__/rez3').toString();
-    expect(flatCompare(rawData)).toEqual(rez3);
+test('compare files', () => {
+  //same files to compare
+  comparation(f1, f1, 'rez1')
+  // emty file comparation
+  comparation(f1, f2, 'rez21');
+  comparation(f2, f1, 'rez22');
+  // different files
+  comparation(f1, f3, 'rez3');
   });
