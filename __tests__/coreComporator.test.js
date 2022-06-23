880e83c414cbc916ac5845a070a962322c5744b9
DenisYagov <denis.yagov@yandex.ru>
2022-06-23 12:34:01 +0500

    added tests step5

--- __tests__/coreComporator.test.js
+++ __tests__/coreComporator.test.js
@@ -2,10 +2,13 @@ import flatCompare from '../src/coreComparator.js';
 import { readFileSync } from 'fs';
 //import { test, expect } from 'jest';
 
-
+// processing files:
 const f1 = '__fixtures__/file1.json';
 const f2 = '__fixtures__/file2.json';
 const f3 = '__fixtures__/file3.json';
+const f4 = '__fixtures__/file4.json';
+const f5 = '__fixtures__/file5.json';
+const f6 = '__fixtures__/file6.json';
 
 const comparation = (file1, file2, eqFile) => {
   const rawData = {
@@ -15,6 +18,7 @@ const comparation = (file1, file2, eqFile) => {
 }
 
 test('compare files', () => {
+  //comaration of json files
   //same files to compare
   comparation(f1, f1, 'rez1')
   // emty file comparation
@@ -22,4 +26,22 @@ test('compare files', () => {
   comparation(f2, f1, 'rez22');
   // different files
   comparation(f1, f3, 'rez3');
+  //comaration of YAML files
+  //same files to compare
+  comparation(f4, f4, 'rez1')
+  // emty file comparation
+  comparation(f4, f5, 'rez21');
+  comparation(f5, f4, 'rez22');
+  // different files
+  comparation(f4, f6, 'rez3');
+  //comaration of mix YAML and json files
+  //same files to compare
+  comparation(f1, f4, 'rez1')
+  // emty file comparation
+  comparation(f4, f2, 'rez21');
+  comparation(f5, f1, 'rez22');
+  // different files
+  comparation(f1, f6, 'rez3');
+
+
   });
