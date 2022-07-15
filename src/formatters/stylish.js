6f1c4d5d644f2e07035df5019d04c4a9df033001
DenisYagov <denis.yagov@yandex.ru>
2022-07-15 10:09:25 +0500

    modification deep: put tabulation symbol vs doublespace

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -7,7 +7,8 @@ const preSym = ['+', '-', '+', '-', ' '];
 const ent = String.fromCharCode(10); // enter symbol
 
 const makeOffset = (offset) => {
-  const outStr = `${offset}${String.fromCharCode(9)}`;
+  // const outStr = `${offset}${String.fromCharCode(9)}`;
+  const outStr = `${offset}  `;
   return outStr;
 };
 
