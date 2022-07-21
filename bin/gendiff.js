31d4ec4704b0bd133e2f84334368865d980527ce
DenisYagov <denis.yagov@yandex.ru>
2022-07-21 14:54:45 +0500

    try to fix bug2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -15,17 +15,18 @@ program
   .option('-f, --format <type>', `output format (default: "${defFormat}")`)
   .option('-h, --help', 'display help for command')
 
-  .action((filepath1, filepath2) => {
+/*  .action((filepath1, filepath2) => {
     const diff = compareFiles(filepath1, filepath2, Command.format);
     console.log(diff);
   });
-/*
+*/
   .action((fileName1, lineOptions, rawData) => {
   // compare files based on input raw data
     // eslint-disable-next-line no-underscore-dangle
     const outStr = compareFiles(rawData.args[0], rawData.args[1], rawData._optionValues.format);
     // in case no input files return null
     (outStr.length !== 0) ? console.log(outStr) : 'files fully compatible';
+    return outStr;
   });
-*/
+
 program.parse();
