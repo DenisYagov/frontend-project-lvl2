55d53955c7defd789445cb0b0c78170059a89518
DenisYagov <denis.yagov@yandex.ru>
2022-07-22 09:27:25 +0500

    rebuilt work with filePath and console args

--- src/coreComparator.js
+++ src/coreComparator.js
@@ -1,5 +1,5 @@
 import { readFileSync } from 'node:fs';
-import { posix } from 'node:path';
+import path, { posix } from 'node:path';
 import parcingFromString from './fileParsing.js';
 import makeStringFromArray from './formatters/index.js';
 // eslint-disable-next-line import/no-named-as-default
@@ -7,20 +7,22 @@ import generateRezultArray from './libComporator.js';
 
 // const path = require('node:path');
 
-const getObjectFromFile = (inputFile) => {
+const getObjectFromFile = (inputFilePath) => {
   // return object from file
   // make absolut path from any type of input
-  const file = posix.resolve(inputFile);
+  // const absolutFilePath = posix.resolve(inputFilePath);
+  const absolutFilePath = path.resolve(process.cwd(), inputFilePath);
+  // path.resolve(process.cwd(), filePath)
   // get extention of the file
-  const fileExt = posix.extname(file).slice(1);
-  const dataStr = readFileSync(file);
-  // const dataStr = readFileSync(inputFile);
+  const fileExt = posix.extname(absolutFilePath).slice(1);
+  const dataStr = readFileSync(absolutFilePath);
+  // const dataStr = readFileSync(inputFilePath);
   return parcingFromString(dataStr, fileExt);
 };
 
 const compareFiles = (file1, file2, format = 'stylish') => {
   // check if all data presented
-  if (file1 === undefined) throw new Error('absent file in input data!');
+  // if (file1 === undefined) throw new Error('absent file in input data!');
   // make objects
   const obj1 = getObjectFromFile(file1);
   const obj2 = getObjectFromFile(file2);
