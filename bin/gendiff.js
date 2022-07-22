1a1b793da2aa9dc05ac3e32a1fd1ebf4fbe7b92c
DenisYagov <denis.yagov@yandex.ru>
2022-07-22 14:29:27 +0500

    try to fix console command bug

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -14,18 +14,21 @@ program
   .option('-V, --version', 'output the version')
   .option('-f, --format <type>', `output format (default: "${defFormat}")`)
   .option('-h, --help', 'display help for command')
-/*
-  .action((filepath1, filepath2) => {
-    const diff = compareFiles(filepath1, filepath2, Command.format);
+
+  .action(() => {
+    // eslint-disable-next-line no-underscore-dangle
+    const diff = compareFiles(program.args[0], program.args[1], program._optionValues.format);
     console.log(diff);
   });
-*/
+/*
   .action((fileName1, lineOptions, rawData) => {
   // compare files based on input raw data
+    console.log('program.format = ', fileName1);
+    console.log('program.format = ', lineOptions);
     // eslint-disable-next-line no-underscore-dangle
     const outStr = compareFiles(rawData.args[0], rawData.args[1], rawData._optionValues.format);
     // in case no input files return null
     (outStr.length !== 0) ? console.log(outStr) : 'files fully compatible';
   });
-
+*/
 program.parse();
